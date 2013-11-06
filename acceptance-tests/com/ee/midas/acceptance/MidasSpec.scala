d3de26a2c687f9a638a9b6d794ced3afb3663e31
Vivek Dhapola <vdhapola@equalexperts.com>
2013-11-06 18:50:35 +0530

    added task smoke-test for midas

--- acceptance-tests/com/ee/midas/acceptance/MidasSpec.scala
+++ acceptance-tests/com/ee/midas/acceptance/MidasSpec.scala
@@ -1,58 +1,63 @@
+package com.ee.midas.acceptance
+
+import org.junit.runner.RunWith
+import org.specs2.runner.JUnitRunner
 import org.specs2.Specification
 import com.mongodb._
 
 // TODO : Write specs for connection failure scenaro
 
-//@RunWith(classOf[JUnitRunner])
-class MidasSpecs extends Specification {
+@RunWith(classOf[JUnitRunner])
+class MidasSpec extends Specification {
 
     var application: MongoClient = null
     var document:DBObject = null
 
-  def is = s2"""
-  Narration:
-  This is a specification to verify that midas behaves as a proxy
-  Assuming that mongods and midas is running
-  A client application should be able to
-      Step 1: Connect to midas             $connect
+   def is = s2"""
+   Narration:
+   This is a specification to verify that midas behaves as a proxy
+   Assuming that mongods and midas is running
+   A client application should
+       Step 1: Connect to midas             $connect
 
-      Step 2: Perform CRUD operations
-      insert documents                     $insert
-      read documents                       $read
-      delete documents                     $delete
+       Step 2: Perform CRUD operations
+           insert documents                 $insert
+           read documents                   $read
+           delete documents                 $delete
 
-      Step 3: Disconnect from midas        $disconnect
+       Step 3: Disconnect from midas        $disconnect
                                                               """
 
   val connect = {
    application = new MongoClient("localhost", 27020)
-   if(application.getConnector.isOpen) "success" else "failure"
+   application.getConnector.isOpen
   }
+
   val insert = {
     document = new BasicDBObject("testName","midas is a proxy")
     val database:DB = application.getDB("midasSmokeTest")
     val collection:DBCollection = database.getCollection("tests")
     val result:WriteResult = collection.insert(document)
-    if(result.getError == null) "success" else "failure"
+   result.getError == null
   }
 
   val read = {
     val database:DB = application.getDB("midasSmokeTest")
     val collection:DBCollection = database.getCollection("tests")
     val readDocument:DBObject = collection.findOne()
-    if(readDocument == document) "success" else "failure"
+    readDocument == document
   }
 
-  def delete = {
+  val delete = {
     val database:DB = application.getDB("midasSmokeTest")
     val collection:DBCollection = database.getCollection("tests")
     val result:WriteResult = collection.remove(document)
-    if(result.getError == null) "success" else "failure"
+    result.getError == null
   }
 
-  def disconnect = {
+  val disconnect = {
     application.close()
-    if(application.getConnector.isOpen) "failure" else "success"
+    application.getConnector.isOpen must beFalse
   }
 
 }

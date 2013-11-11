b8b19538e6c051628b7d5a010fa0600cecde5ae2
Dhaval Dalal <dhaval.dalal@software-artisan.com>
2013-11-11 18:06:39 +0530

    - clean up

--- src/main/scala/com/ee/midas/pipes/SocketConnector.scala
+++ src/main/scala/com/ee/midas/pipes/SocketConnector.scala
@@ -3,15 +3,15 @@ package com.ee.midas.pipes
 import java.net.Socket
 
 class SocketConnector private (source: Socket) {
-  def <==> (target: Socket) : DuplexPipe = DuplexPipe(==> (target), <== (target))
+  def <==> (target: Socket) = DuplexPipe(==> (target), <== (target))
 
-  def ==> (target: Socket) : SimplexPipe = {
+  def ==> (target: Socket) = {
     val srcIn = source.getInputStream
     val tgtOut = target.getOutputStream
     new SimplexPipe("==>", srcIn, tgtOut)
   }
 
-  def <== (target: Socket) : SimplexPipe = {
+  def <== (target: Socket) = {
     val tgtIn = target.getInputStream
     val srcOut = source.getOutputStream
     new SimplexPipe("<==", tgtIn, srcOut)

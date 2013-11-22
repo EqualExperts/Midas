183fa665bdbc495b77aade9a414d06629ce3e0c1
Dhaval Dalal <dhaval.dalal@software-artisan.com>
2013-11-22 14:31:13 +0530

    - Made Versioner Loggable

--- src/main/scala/com/ee/midas/transform/Versioner.scala
+++ src/main/scala/com/ee/midas/transform/Versioner.scala
@@ -2,8 +2,9 @@ package com.ee.midas.transform
 
 import org.bson.BSONObject
 import DocumentOperations._
+import com.ee.midas.utils.Loggable
 
-trait Versioner {
+trait Versioner extends Loggable {
 
   def getVersion(document: BSONObject)(implicit transformType: TransformType) = {
     val versionFieldName = transformType.versionFieldName()
@@ -19,17 +20,17 @@ trait Versioner {
     val versionFieldName = transformType.versionFieldName()
     getVersion(document) match {
       case Some(version) => {
-        println("Current Version %d of Document %s".format(version, document))
+        log.info("Current Version %d of Document %s".format(version, document))
         val nextVersion = version + 1
         document + (versionFieldName, nextVersion)
-        println("Updated Version to %d on Document %s\n".format(nextVersion, document))
+        log.info("Updated Version to %d on Document %s\n".format(nextVersion, document))
         document
       }
       case None => {
-        println("No Versioning found on Document %s".format(document))
+        log.info("No Versioning found on Document %s".format(document))
         val version = 1
         document + (versionFieldName, version)
-        println("Added Version %d to Document %s\n".format(version, document))
+        log.info("Added Version %d to Document %s\n".format(version, document))
         document
       }
     }

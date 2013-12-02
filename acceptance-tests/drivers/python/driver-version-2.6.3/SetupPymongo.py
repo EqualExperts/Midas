fce3e7105353e5712d545f4e445755051121c4df
Dhaval Dalal <dhaval.dalal@software-artisan.com>
2013-12-02 11:00:09 +0530

    - Made sure that setup works on Mac OS X

--- acceptance-tests/drivers/python/driver-version-2.6.3/SetupPymongo.py
+++ acceptance-tests/drivers/python/driver-version-2.6.3/SetupPymongo.py
@@ -1,6 +1,13 @@
+import sys
 import os
 
-if os.name == 'nt':
+print("Platform = " + sys.platform)
+platform = sys.platform
+if platform == 'nt':
     os.system("easy_install pymongo")
-else :
+elif (platform == 'linux2'):
     os.system("pip install pymongo==2.6.3")
+elif(platform == 'darwin'):
+    os.system("sudo python -m easy_install pymongo")
+else:
+    print("Please add your platform here as in the current scheme it is not applicable")

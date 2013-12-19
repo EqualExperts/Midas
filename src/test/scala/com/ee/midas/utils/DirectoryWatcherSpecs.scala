af0eda268b8cb8b3f62478947c15f11c13b416fc
Vivek Dhapola <vdhapola@equalexperts.com>
2013-12-19 17:07:56 +0530

    added specs to check DirectoryWatcher for bulk creation and deletion.

--- src/test/scala/com/ee/midas/utils/DirectoryWatcherSpecs.scala
+++ src/test/scala/com/ee/midas/utils/DirectoryWatcherSpecs.scala
@@ -1,10 +1,10 @@
 package com.ee.midas.utils
 
-import java.io.File
+import java.io.{FileWriter, File}
 import org.junit.runner.RunWith
 import org.mockito.runners.MockitoJUnitRunner
 import org.specs2.matcher.JUnitMustMatchers
-import org.junit.Test
+import org.junit.{Before, Test}
 
 /**
  * IMPORTANT NOTE:
@@ -21,27 +21,131 @@ import org.junit.Test
 class DirectoryWatcherSpecs extends JUnitMustMatchers{
 
   def waitForWatcherToStart(millis: Long) = Thread.sleep(millis)
+  val path: String = "/" + System.getProperty("user.dir")
 
   @Test
-  def watchAnEventForADirectory() {
-    val path = "/" + System.getProperty("user.dir")
+  def itWatchesACreateEventForADirectory() {
+    //given: a watcher with a directory to watch
     var watching: Boolean = false
     val watcher = new DirectoryWatcher(path)(watchEvent => {
         watching = true
       }
     )
     watcher.start
-    val newFile = new File("dummyFile.txt")
-    newFile.createNewFile()
-    newFile.deleteOnExit()
+
+    //when: there is a create event
+    val file = new File("createFile.txt")
+    file.createNewFile()
+    file.deleteOnExit()
     waitForWatcherToStart(200)
     watcher.stopWatching
+
+    //then: it was captured by the watcher
     watching must beTrue
   }
 
   @Test
+  def itWatchesAModifyEventForADirectory() {
+    //given: a watcher with a directory to watch, and an existing file in the directory
+    val file = new File("modifyFile.txt")
+    file.createNewFile()
+    file.deleteOnExit()
+    var watching: Boolean = false
+    val watcher = new DirectoryWatcher(path)(watchEvent => {
+      watching = true
+    })
+    watcher.start
+
+    //when: there is a modify event
+    val writer = new FileWriter(file)
+    writer.write("add some data.")
+    writer.close()
+
+    waitForWatcherToStart(200)
+    watcher.stopWatching
+
+    //then: it was captured by the watcher
+    watching must beTrue
+  }
+
+  @Test
+  def itWatchesADeleteEventForADirectory() {
+    //given: a watcher with a directory to watch, and an existing file in the directory
+    val file = new File("deleteFile.txt")
+    file.createNewFile()
+    var watching: Boolean = false
+    val watcher = new DirectoryWatcher(path)(watchEvent => {
+      watching = true
+    })
+    watcher.start
+
+    //when: there is a modify event
+    file.delete()
+
+    waitForWatcherToStart(200)
+    watcher.stopWatching
+
+    //then: it was captured by the watcher
+    watching must beTrue
+  }
+
+  @Test
+  def itWatchesEventsOnMultipleFilesForADirectory() {
+    //given: a watcher with a directory to watch
+    var watching: Int = 0
+    val watcher = new DirectoryWatcher(path)(watchEvent => {
+      watching += 1
+    }
+    )
+    watcher.start
+
+    //when: there is a create event
+    val file1 = new File("createFile1.txt")
+    file1.createNewFile()
+    file1.deleteOnExit()
+    val file2 = new File("createFile2.txt")
+    file2.createNewFile()
+    file2.deleteOnExit()
+    val file3 = new File("createFile3.txt")
+    file3.createNewFile()
+    file3.deleteOnExit()
+    waitForWatcherToStart(200)
+    watcher.stopWatching
+
+    //then: it was captured by the watcher
+    watching === 3
+  }
+
+  @Test
+  def itWatchesDeleteEventsOnMultipleFilesForADirectory() {
+    //given: a watcher with a directory to watch
+    var deleted: Int = 0
+    val file1 = new File("deleteFile1.txt")
+    file1.createNewFile()
+    val file2 = new File("deleteFile2.txt")
+    file2.createNewFile()
+    val file3 = new File("deleteFile3.txt")
+    file3.createNewFile()
+
+    val watcher = new DirectoryWatcher(path)(watchEvent => {
+      deleted += 1
+    }
+    )
+    watcher.start
+
+    //when: there is a create event
+    file1.delete()
+    file2.delete()
+    file3.delete()
+    waitForWatcherToStart(200)
+    watcher.stopWatching
+
+    //then: it was captured by the watcher
+    deleted === 3
+  }
+
+  @Test
   def stopWatchingADirectoryWhenRequested() {
-    val path = "/" + System.getProperty("user.dir")
     var watching: Boolean = false
     val watcher = new DirectoryWatcher(path)(watchEvent => {
         watching = true
@@ -54,13 +158,12 @@ class DirectoryWatcherSpecs extends JUnitMustMatchers{
 
   @Test
   def stopWatchingADirectoryInCaseOfAnException() {
-    val path = "/" + System.getProperty("user.dir")
     val watcher = new DirectoryWatcher(path)(watchEvent => {
         throw new Exception("Exception forcibly throw from within a test case.")
       }
     )
     watcher.start
-    val file = new File("exception.txt")
+    val file = new File("exceptionFile.txt")
     file.createNewFile()
     file.deleteOnExit()
     waitForWatcherToStart(200)

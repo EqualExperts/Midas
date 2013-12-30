223837a9d9f3824f68184ee03652742164b2857f
Vivek Dhapola <vdhapola@equalexperts.com>
2013-12-30 18:57:24 +0530

    Added exception handling and retry logic for reading files

--- src/main/groovy/com/ee/midas/dsl/interpreter/Reader.groovy
+++ src/main/groovy/com/ee/midas/dsl/interpreter/Reader.groovy
@@ -3,10 +3,12 @@ package com.ee.midas.dsl.interpreter
 import com.ee.midas.dsl.interpreter.ast.StatementTransformation
 import com.ee.midas.dsl.interpreter.representation.Tree
 import groovy.transform.CompileStatic
+import groovy.util.logging.Slf4j
 import org.codehaus.groovy.control.CompilerConfiguration
 import org.codehaus.groovy.control.customizers.ASTTransformationCustomizer
 import org.codehaus.groovy.control.customizers.SecureASTCustomizer
 
+@Slf4j
 public class Reader {
     public Reader() {
     }
@@ -44,15 +46,38 @@ public class Reader {
 
     public Tree read(final List<File> deltaFiles) {
         def parser = new Parser()
+        def maxRetries = 5
         deltaFiles.each { deltaFile ->
-            def deltaFileName = deltaFile.name
-            def dsl = deltaFile.text
-            def code = """{-> $dsl}"""
-            //shell evaluates once, hence create new each time
-            def shell = createNewShell()
-            def delta = shell.evaluate(code, deltaFileName)
-            parser.parse(delta)
+            readSafely(deltaFile, maxRetries, { file ->
+                def deltaFileName = file.name
+                def dsl = file.text
+                def code = """{-> $dsl}"""
+                //shell evaluates once, hence create new each time
+                def shell = createNewShell()
+                def delta = shell.evaluate(code, deltaFileName)
+                parser.parse(delta)
+            })
         }
         parser.ast()
+    }
+
+    private void readSafely(File deltaFile, int maxRetries, Closure closure) {
+        def fileRead = false
+        def numOfRetries = 0
+        while (deltaFile.exists() && !fileRead) {
+            try {
+                closure.call(deltaFile)
+                fileRead = true
+            } catch (FileNotFoundException exception) {
+                if(numOfRetries < maxRetries) {
+                    numOfRetries++
+                    log.error("${exception.getClass().getName()} occurred: ${exception.message}, retrying $numOfRetries out of $maxRetries.")
+                } else {
+                    log.error("${exception.getClass().getName()} occurred: ${exception.message}, retries maxed out, skipping file ${deltaFile.name}.")
+                    break;
+                }
+            }
+        }
+        deltaFile.exists() ?: log.error("File ${deltaFile.name} doesn't exist.")
     }
 }

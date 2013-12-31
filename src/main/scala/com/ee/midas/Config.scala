2fb2738e6d5071257b2c194cf16c2c00ace97fb8
Komal <kjain@equalexperts.com>
2013-12-31 17:06:21 +0530

    Changed default value for deltaDir

--- src/main/scala/com/ee/midas/Config.scala
+++ src/main/scala/com/ee/midas/Config.scala
@@ -2,4 +2,4 @@ package com.ee.midas
 
 import com.ee.midas.transform.TransformType
 
-case class Config (midasHost:String = "localhost", midasPort: Int = 27020 , mongoHost: String = "localhost", mongoPort: Int = 27017, mode:TransformType = TransformType.EXPANSION, deltasDir: String = "deltas/")
+case class Config (midasHost:String = "localhost", midasPort: Int = 27020 , mongoHost: String = "localhost", mongoPort: Int = 27017, mode:TransformType = TransformType.EXPANSION, deltasDir: String = "deltas")

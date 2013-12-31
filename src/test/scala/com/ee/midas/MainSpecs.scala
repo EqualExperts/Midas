43027bc6d79d608be4c58ebba0e91a207a2a332a
Komal <kjain@equalexperts.com>
2013-12-31 14:14:46 +0530

    Added specs to test processCLI parameters

--- src/test/scala/com/ee/midas/MainSpecs.scala
+++ src/test/scala/com/ee/midas/MainSpecs.scala
@@ -20,5 +20,101 @@ class MainSpecs extends Specification{
          deltasURI mustEqual "deltas/"
        }
 
+      " runs on a given PORT and connects to default source and mongoPort" in {
+        val (midasHost: String, midasPort: Int, mongoHost: String, mongoPort: Int,
+        transformType: TransformType, deltasURI: String) = Main.processCLIparameters(Array("--port","27040"))
+
+        midasHost mustEqual "localhost"
+        midasPort mustEqual 27040
+        mongoHost mustEqual "localhost"
+        mongoPort mustEqual 27017
+        transformType mustEqual TransformType.EXPANSION
+        deltasURI mustEqual "deltas/"
+      }
+
+      " runs on default port and connects to given MONGOHOST on default mongoPort" in {
+        val (midasHost: String, midasPort: Int, mongoHost: String, mongoPort: Int,
+        transformType: TransformType, deltasURI: String) = Main.processCLIparameters(Array("--source","192.168.1.44"))
+
+        midasHost mustEqual "localhost"
+        midasPort mustEqual 27020
+        mongoHost mustEqual "192.168.1.44"
+        mongoPort mustEqual 27017
+        transformType mustEqual TransformType.EXPANSION
+        deltasURI mustEqual "deltas/"
+      }
+
+      " runs on default port and connects to default mongoHost on given MONGOPORT" in {
+        val (midasHost: String, midasPort: Int, mongoHost: String, mongoPort: Int,
+        transformType: TransformType, deltasURI: String) = Main.processCLIparameters(Array("--mongoPort","27019"))
+
+        midasHost mustEqual "localhost"
+        midasPort mustEqual 27020
+        mongoHost mustEqual "localhost"
+        mongoPort mustEqual 27019
+        transformType mustEqual TransformType.EXPANSION
+        deltasURI mustEqual "deltas/"
+      }
+
+      " runs on given PORT and connects to given MONGOHOST on default mongoPort" in {
+        val (midasHost: String, midasPort: Int, mongoHost: String, mongoPort: Int,
+        transformType: TransformType, deltasURI: String) = Main.processCLIparameters(Array("--port","27040","--source","192.168.1.44"))
+
+        midasHost mustEqual "localhost"
+        midasPort mustEqual 27040
+        mongoHost mustEqual "192.168.1.44"
+        mongoPort mustEqual 27017
+        transformType mustEqual TransformType.EXPANSION
+        deltasURI mustEqual "deltas/"
+      }
+
+      " runs on given PORT and connects to default mongoHost on given MONGOPORT" in {
+        val (midasHost: String, midasPort: Int, mongoHost: String, mongoPort: Int,
+        transformType: TransformType, deltasURI: String) = Main.processCLIparameters(Array("--port","27040","--mongoPort","27019"))
+
+        midasHost mustEqual "localhost"
+        midasPort mustEqual 27040
+        mongoHost mustEqual "localhost"
+        mongoPort mustEqual 27019
+        transformType mustEqual TransformType.EXPANSION
+        deltasURI mustEqual "deltas/"
+      }
+
+      " runs on default port and connects to given MONGOHOST on MONGOPORT" in {
+        val (midasHost: String, midasPort: Int, mongoHost: String, mongoPort: Int,
+        transformType: TransformType, deltasURI: String) = Main.processCLIparameters(Array("--source","192.168.1.44","--mongoPort","27019"))
+
+        midasHost mustEqual "localhost"
+        midasPort mustEqual 27020
+        mongoHost mustEqual "192.168.1.44"
+        mongoPort mustEqual 27019
+        transformType mustEqual TransformType.EXPANSION
+        deltasURI mustEqual "deltas/"
+      }
+
+      " runs in CONTRACTION mode" in {
+        val (midasHost: String, midasPort: Int, mongoHost: String, mongoPort: Int,
+        transformType: TransformType, deltasURI: String) = Main.processCLIparameters(Array("--port","27040","--source","192.168.1.44","--mongoPort","27019","--mode","CONTRACTION"))
+
+        midasHost mustEqual "localhost"
+        midasPort mustEqual 27040
+        mongoHost mustEqual "192.168.1.44"
+        mongoPort mustEqual 27019
+        transformType mustEqual TransformType.CONTRACTION
+        deltasURI mustEqual "deltas/"
+      }
+
+      " uses the specified directory for picking up delta files " in {
+        val (midasHost: String, midasPort: Int, mongoHost: String, mongoPort: Int,
+        transformType: TransformType, deltasURI: String) = Main.processCLIparameters(Array("--port","27040",
+          "--source","192.168.1.44","--mongoPort","27019","--mode","CONTRACTION","--deltasDir","newDir/NewDelta"))
+
+        midasHost mustEqual "localhost"
+        midasPort mustEqual 27040
+        mongoHost mustEqual "192.168.1.44"
+        mongoPort mustEqual 27019
+        transformType mustEqual TransformType.CONTRACTION
+        deltasURI mustEqual "newDir/NewDelta"
+      }
     }
 }

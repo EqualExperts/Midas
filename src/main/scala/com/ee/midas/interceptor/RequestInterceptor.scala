9ee6777e977a546b99e2956d05e36e6347b3602b
Komal <kjain@equalexperts.com>
2014-02-03 15:05:43 +0530

    Decoded update query

--- src/main/scala/com/ee/midas/interceptor/RequestInterceptor.scala
+++ src/main/scala/com/ee/midas/interceptor/RequestInterceptor.scala
@@ -3,6 +3,8 @@ package com.ee.midas.interceptor
 import java.io.InputStream
 import com.ee.midas.utils.Loggable
 import scala.util.control.Breaks.break
+import org.bson.BasicBSONDecoder
+import com.mongodb.{DefaultDBDecoder, DBDecoder}
 
 class RequestInterceptor (tracker: MessageTracker) extends MidasInterceptable with Loggable {
 
@@ -23,6 +25,23 @@ class RequestInterceptor (tracker: MessageTracker) extends MidasInterceptable wi
       case OP_QUERY | OP_GET_MORE => {
         val fullCollectionName = toFullCollectionName(remaining)
         tracker.track(header.requestID, fullCollectionName)
+      }
+      case OP_UPDATE => {
+        val fullCollectionName = toFullCollectionName(remaining)
+        println("In update query......... full collection name == "+fullCollectionName)
+        println(" collection length == "+fullCollectionName.length+" remaining== "+remaining.length)
+        val decoder: DBDecoder = new DefaultDBDecoder()
+        var i = fullCollectionName.length
+        var j = 0
+        val payload:Array[Byte] = new Array[Byte](remaining.length - (fullCollectionName.length+5))
+        for(i <- (fullCollectionName.length+5) until remaining.length )
+        {
+          payload(j) = remaining(i)
+          j= j+1
+        }
+
+        val dbObject = decoder.decode(payload,null)
+        println("Decoded object == "+dbObject)
       }
       case _ => ""
     }

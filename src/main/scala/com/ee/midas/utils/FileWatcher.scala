695710af51af6b1a589f0ca2f7c677a09c865587
Dhaval Dalal <dhaval.dalal@software-artisan.com>
2014-02-20 16:41:24 +0530

    - Converted FileWatcher to a trait

--- src/main/scala/com/ee/midas/utils/FileWatcher.scala
+++ src/main/scala/com/ee/midas/utils/FileWatcher.scala
@@ -3,18 +3,17 @@ package com.ee.midas.utils
 import org.apache.log4j.helpers.{FileWatchdog, LogLog}
 import scala.concurrent.duration.TimeUnit
 
-class FileWatcher(fileName: String, watchEvery: Int, watchUnit: TimeUnit)(actionOnModify: () => Unit) {
-
-  val watcher = new FileWatchdog(fileName: String) {
-    delay = watchUnit.toMillis(watchEvery)
-    LogLog.debug(s"watching file $fileName for any modifications.")
-    def doOnChange() = {
-      LogLog.debug(s"Change detected for $fileName. Performing the specified action.")
-      actionOnModify()
-      LogLog.debug(s"Action performed successfully.")
+trait FileWatcher {
+  def watch(fileName: String, watchEvery: Int, watchUnit: TimeUnit)(actionOnModify: => Any): Unit = {
+    val watcher = new FileWatchdog(fileName: String) {
+      delay = watchUnit.toMillis(watchEvery)
+      LogLog.debug(s"watching file $fileName for any modifications.")
+      def doOnChange = {
+        LogLog.debug(s"Change detected for $fileName. Performing the specified action.")
+        actionOnModify
+        LogLog.debug(s"Action performed successfully.")
+      }
     }
+    watcher.start()
   }
-
-  def start() = watcher.start()
-
 }
--- src/test/scala/com/ee/midas/utils/FileWatcherSpecs.scala
+++ src/test/scala/com/ee/midas/utils/FileWatcherSpecs.scala
@@ -1,60 +1,65 @@
 package com.ee.midas.utils
 
-import org.specs2.mutable.Specification
+import org.specs2.mutable.{Specification}
 import java.io.{FileWriter, File}
 import java.util.concurrent.TimeUnit
+import org.specs2.specification.{Scope}
+import org.junit.runner.RunWith
+import org.specs2.runner.JUnitRunner
 
+@RunWith(classOf[JUnitRunner])
 class FileWatcherSpecs extends Specification {
 
-  sequential
+  trait FileWatching extends FileWatcher with Scope {
+    def createFile(fileName: String): File = {
+      val file = new File(fileName)
+      if (!file.exists()) file.createNewFile()
+      file.deleteOnExit()
+      file
+    }
+  }
+
 
   def waitForWatcherToWatch(millis: Int) ={
     Thread.sleep(millis)
   }
 
+  sequential
   "File Watcher" should {
 
-    "perform given action once during initialization" in {
-      //given: A file
-        val file = new File("sample1.config")
-        if(!file.exists()) file.createNewFile()
-        file.deleteOnExit()
-        var timesExecuted = 0
-        def watchAction() {
-          timesExecuted += 1
-        }
+    "watch file and execute given action once during initialization" in new FileWatching {
+      //given
+      val file = createFile("sample1.config")
+      var timesExecuted = 0
 
-      //when: A FileWatcher is initialized
-        new FileWatcher(file.getAbsolutePath, 1, TimeUnit.SECONDS)(watchAction)
+      //when
+      watch(file.getAbsolutePath, 1, TimeUnit.SECONDS) {
+        timesExecuted += 1
+      }
 
       //then: the action was performed once
-        timesExecuted mustEqual 1
+      timesExecuted mustEqual 1
     }
 
-    "perform given action whenever the file is modified" in {
+    "watch file and execute given action whenever the file is modified" in new FileWatching {
       //given: A file
-        val file = new File("sample2.config")
-        if(!file.exists()) file.createNewFile()
-        file.deleteOnExit()
-        var timesExecuted = 0
-        def watchAction() {
-          timesExecuted += 1
-        }
+      val file = createFile("sample2.config")
+      var timesExecuted = 0
 
       //and: a FileWatcher running on it
-        val watcher = new FileWatcher(file.getAbsolutePath, 5, TimeUnit.MILLISECONDS)(watchAction)
-        watcher.start()
+      watch(file.getAbsolutePath, 30, TimeUnit.MILLISECONDS) {
+        timesExecuted += 1
+      }
 
       //when: the file is modified
-        val writer = new FileWriter(file)
-        writer.write("some random data.")
-        writer.flush()
-        writer.close()
+      val writer = new FileWriter(file)
+      writer.write("some random data.")
+      writer.flush()
+      writer.close()
 
       //then: the action was performed twice
-        waitForWatcherToWatch(20)
-        timesExecuted mustEqual 2
+      waitForWatcherToWatch(100)
+      timesExecuted mustEqual 2
     }
-
   }
 }

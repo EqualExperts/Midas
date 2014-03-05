6fd9b3a9c5ffee774e15561eca92e5d4a5a179eb
Dhaval Dalal <dhaval.dalal@software-artisan.com>
2014-03-05 12:49:43 +0530

    - changed year

--- distribution-template/midas.sh
+++ distribution-template/midas.sh
@@ -1,5 +1,5 @@
 ###############################################################################
- # Copyright (c) 2013, Equal Experts Ltd
+ # Copyright (c) 2014, Equal Experts Ltd
  # All rights reserved.
  #
  # Redistribution and use in source and binary forms, with or without
